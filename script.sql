629397cd3e36df33103ea06b7b440fa7311bc9b7
Admin <admin@test.local>
2020-08-28 12:25:34 +0000

    Commit

--- script.sql
+++ script.sql
@@ -1,11 +1,10 @@
-set colsep ','
-set headsep off
 set pagesize 0
-set trimspool on
+set term off
+set feed off
 
 spool users.csv
 
-SELECT * FROM USERS;
+SELECT ID||','||USER_NAME||','||USER_IS_ACTIVE||','||USER_IS_BLOCKED||','||USER_EMAIL||','||USER_REGISTRATION_DATE||','||ROLE_FK||','||FILE_FK||','||USER_IS_DELETED FROM USERS;
 
 spool off;
 exit;
