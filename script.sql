b6ca97e26c7776a1d1851397f92f60d4f5a90011
Admin <admin@test.local>
2020-08-28 11:53:50 +0000

    Commit

--- script.sql
+++ script.sql
@@ -1,1 +1,15 @@
-select * from v$version;
+set colsep ,
+set headsep off
+set pagesize 0
+set trimspool on
+set linesize 2
+set numwidth 5
+
+spool users.csv
+
+SELECT ID, USER_NAME, USER_PASSWORD, USER_IS_ACTIVE, USER_IS_BLOCKED, USER_EMAIL, USER_REGISTRATION_DATE, ROLE_FK, FILE_FK, USER_SALT, USER_IS_DELETED
+FROM USERS;
+
+spool off;
+exit;
+
