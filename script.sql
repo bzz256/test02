e6f3ed1a06b169af6c30ce5a4ac8098b3088c1a6
Admin <admin@test.local>
2020-08-28 12:02:15 +0000

    Commit

--- script.sql
+++ script.sql
@@ -1,16 +1,11 @@
-set colsep ','
-set echo off
-set feedback off
-set linesize 1000
-set pagesize 0
-set sqlprompt ''
-set trimspool on
+set colsep ,
 set headsep off
+set pagesize 0
+set trimspool on
 
 spool users.csv
 
-SELECT ID, USER_NAME, USER_PASSWORD, USER_IS_ACTIVE, USER_IS_BLOCKED, USER_EMAIL, USER_REGISTRATION_DATE, ROLE_FK, FILE_FK, USER_SALT, USER_IS_DELETED
-FROM USERS;
+SELECT * FROM USERS;
 
 spool off;
 exit;
